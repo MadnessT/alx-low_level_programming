e4a4cace2d786451fcd4de5799c11cb271caa98a
MadnessT <vuyanind1@gmail.com>
2022-07-02 03:03:18 -0700

    printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <sudio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcme is a piece of work,\n");
+	return (0);
+}
